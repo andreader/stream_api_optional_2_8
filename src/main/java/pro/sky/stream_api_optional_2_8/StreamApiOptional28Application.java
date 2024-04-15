292a8b74a9b62ebb961ce40d5412945c14aa9cb4
Андрей <79538233369@ya.ru>
2024-04-15 18:45:33 +0500

    Master

---
+++ src/main/java/pro/sky/stream_api_optional_2_8/StreamApiOptional28Application.java
@@ -0,0 +1,13 @@
+package pro.sky.stream_api_optional_2_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiOptional28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiOptional28Application.class, args);
+    }
+
+}
